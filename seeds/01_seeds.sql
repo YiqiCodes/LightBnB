f7ee514222a928eee6dee391ff7f5c7c08716ecf
Tyler <yiqizhang@gmail.com>
2020-02-05 20:52:02 +0000

    added seeds//info for database

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (name, email, password)
+VALUES ('Yiqi', 'c0rn_l0v3r@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
+    VALUES (
+    'At taught', 'Establish suppose exciting hand each live exactly physical subject greater biggest result longer cold parallel what movement tax draw poor brief president express pipe provide coffee sky chicken east yes rice onto control would higher fort river canal surprise nearly settle teacher saw cup floating apple affect strip everything mental else when warm palace fuel box dream tape cloth indicate chain five whenever material toward uncle any lost rocky doing taste desert had improve map near light yourself discuss quickly fat bread job speak born powder taken foreign asleep ever try solar drink purpose run involved pass happened safe shorter corner draw cool twelve division under creature jar likely rough judge state like after parallel substance sent review floating angry image mile original potatoes sea whom please shout load judge they available individual wish too team indicate acres pole five itself exactly essential constantly sale face least donkey potatoes window composition old rabbit off out band six tank soon flew later us include police story dull young twenty steady onto map beyond each meat bus climate eleven what spring we gain therefore share particles guard moving mix thee darkness collect from purple course activity forget wet such damage main yellow question stronger accurate meet twice caught walk shade law truth nuts pencil cannot dawn girl last unhappy weather train trick recently him child friend stems climb throughout put zipper damage labor root whistle volume experience larger smile run onto himself glass cell cover heart park gulf still shelter rich fur doll zero most bat note applied dream wheel class story syllable similar several learn donkey term type year some short composition so pride zipper piano cave shape castle ask box direct speed major sets every alphabet chemical column characteristic could lost ready simplest skill given join newspaper television cold tropical', 776, 'https://images.pexels.com/photos/1438834/pexels-photo-1438834.jpeg', 'https://images.pexels.com/photos/1438834/pexels-photo-1438834.jpeg?auto=compress&cs=tinysrgb&h=350', 54361, 1, 9, 6, true, 'Manitoba', 'Dauphin', 'Canada', '1513 Izesuz Terrace', '83021');
+
+INSERT INTO reservations (
+    id, guest_id, property_id, start_date, end_date) 
+    VALUES (
+    1013176, 29, 250, '2023-07-10', '2023-07-17');
+
+INSERT INTO property_reviews (
+    guest_id, property_id, reservation_id, rating, message) 
+    VALUES (
+    714, 992, 1017476, 5, 'Cufowuj mup caefgak kodebu usmozbo logona mi ajki cotpaj taf ho boftare ufi.');
